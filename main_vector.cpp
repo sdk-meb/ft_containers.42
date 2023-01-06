c831224319560ebb32754f1a9c033787233c5454
mes-sadk <use2ess@yahoo.com>
2023-01-06 19:19:13 +0100

    vactor insertion should work likely, iter != rev_iter

--- main_vector.cpp
+++ main_vector.cpp
@@ -28,7 +28,7 @@ int main( )
    v3.push_back(40);
    v3.push_back(50);
 
-
+   v2.push_back(10);
    v2.push_back(20);
    v2.push_back(30);
    v2.push_back(40);
@@ -41,11 +41,37 @@ int main( )
 
    v3.insert(v3.end() ,v3.begin(), v3.end() -1);
 
-   ft::vector<int>::iterator iter3 = v3.end() -= 1;
-   std::vector<int>::iterator iter2 = v2.end() -= 1;
+   v2.insert(v2.end() ,1, 0);
 
-   std::cout <<  *iter3.base() << std::endl;
-   std::cout <<  *iter2.base() << std::endl;
+   v3.insert(v3.end() ,1,0);
+
+   ft::vector<int>::iterator iter3 = v3.begin() -= 0;
+   std::vector<int>::iterator iter2 = v2.begin() -= 0;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
+
+   std::cout <<  *(iter3++).base() << std::endl;
+   std::cout <<  *(iter2++).base() << std::endl;
 
 
 }
